4592827aff36532ec3bafa09e0662e6ebc0630cd
Snehasish Ghosh <sgtracer@gmail.com>
2023-10-07 17:36:45 +0530

    update

--- application.py
+++ application.py
@@ -74,7 +74,43 @@ def profiletags():
 
     return jsonify(points)
 
-# @app.route('/api/tags/project',methods=['POST'])
+@app.route('/api/tags/project',methods=['POST'])
+def projecttag():
+    received_data=request.get_json()
+    username=received_data['username']
+    title=received_data['title']
+    abstract=received_data['abstract']
+    question="Title:'"+title+"'\n\nAbstract: "+abstract
+    engineeredprompt=question+'\n'+"Make EXACTLY 20 tags which can be added to this project according to the specified title and abstract\n\n Response: MUST BE JSON(tag1: 'tag1', tag2: 'tag2', tag3: 'tag3', tag4: 'tag4', tag5: 'tag5', tag6: 'tag6', tag7: 'tag7', tag8: 'tag8', tag9: 'tag9', tag10: 'tag10', tag11: 'tag11', tag12: 'tag12', tag13: 'tag13', tag14: 'tag14', tag15: 'tag15', tag16: 'tag16', tag17: 'tag17', tag18: 'tag18', tag19: 'tag19', tag20: 'tag20')"
+    questionreply=openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k-0613",
+            temperature=1,
+
+            messages=[
+                    {"role": "system", "content": "You're supposed to be creative while STRICTLY following instructions."},
+                    {"role": "user", "content": f"{engineeredprompt}"}
+            ],       
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+    i=1
+    points=[]
+    while True:
+        replytext=questionreply.choices[0].message.content
+        try:
+            points.append(json.loads(replytext)[f"tag{i}"])
+            print(points[i-1])
+        except:
+             pass
+        if i==20:
+            break
+        i+=1
+
+    return jsonify(points)
+
+
 @app.route('/home',methods=['GET','POST'])
 def home():
     if request.method=='POST':
